ee618dac71b2856761b64a666d6afdaea1dbe76f
Evelyn dos Santos <evelynsantos@ibm.com>
2022-09-27 11:22:11 -0300

    :sparkles: Add configurações :sparkles:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarModule } from './components/navbar/navbar.component.module';
+import { AnimesComponent } from './components/animes/animes.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimesComponent,
   ],
   imports: [
-    BrowserModule
+    NavbarModule,
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
